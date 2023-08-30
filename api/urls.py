970cb095584935affb8e9fe613ad3fbf8a4944ab
Ravshanbekk07 <ramazonof07@gmail.com>
2023-08-30 14:21:41 +0500

    cool

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import get_all,create,get_id
+from .views import get_all,create,get_id,update
 
 urlpatterns=[
     path('',get_all),
     path('create',create),
     path('<int:pk>',get_id),
+    path('<int:pk>/update',update),
 ]
 
 
