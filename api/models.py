07618c1f1fcd66f1a351df36762804e8f102ee44
Ravshanbekk07 <ramazonof07@gmail.com>
2023-08-30 10:23:55 +0500

    migare

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=30)
+    price=models.FloatField()
+    quantity=models.IntegerField()
+    description=models.TextField(blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateField(auto_now=True)
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
-from .views import get_all
+
  
 
 
-urlpatterns = [
-    path('products/all',get_all)
-]
